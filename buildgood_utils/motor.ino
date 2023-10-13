2bfbdece26abcbfd927d8649d307c09193205704
Sophont-Prime <147148655+Sophont-Prime@users.noreply.github.com>
2023-10-13 13:25:34 +0100

    Update motor.ino

--- buildgood_utils/motor.ino
+++ buildgood_utils/motor.ino
@@ -1,11 +1,16 @@
 #include <Arduino.h>
 
-Define M1D1 4; //Direction1 for Motor 1
-Define M1D2 5; //Direction2 for Motor 1
-Define M1S 6; //Speed for Motor 1
-Define M2D1 7; //Direction1 for Motor 2
-Define M2D2 8; //Direction2 for Motor 2
-Define M2S 9; //Speed for Motor 2
+Define M1D1 D4; //Direction1 for Motor 1
+Define M1D2 D5; //Direction2 for Motor 1
+Define M1S D6; //Speed for Motor 1
+Define E1F A1;
+Define E1B A2;
+
+Define M2D1 D7; //Direction1 for Motor 2
+Define M2D2 D8; //Direction2 for Motor 2
+Define M2S D9; //Speed for Motor 2
+Define E2F A3;
+Deinfe E2B A4;
 
 /*
 Motors require one of the Direction pins to be High and one to be Low to define direction.
@@ -13,16 +18,29 @@ The Speed pin must be a PWM signal that defines the speed. If the signal is too 
 */
 
 void setup(){
+  Serial.begin(9600);
   pinMode(M1D1, OUTPUT);
   pinMode(M1D2, OUTPUT);
   pinMode(M1S, OUTPUT);
+  
   pinMode(M2D1, OUTPUT);
   pinMode(M2D2, OUTPUT);
   pinMode(M2S, OUTPUT);
+  
   Serial.println("Setup successful, don't panic");
   
 }
 
+void forwards(){
+  
+}
+
+void setspeed(){
+M1S = 
+}
+
 void loop(){
-  if  
+  Serial.begin(9600);
+  
+  Serial.println("Not this file!");  
 }
