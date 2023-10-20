3d8976b72abce598a123b339c8cb4f407251b276
Sophont-Prime <147148655+Sophont-Prime@users.noreply.github.com>
2023-10-20 10:51:53 +0100

    Update motor.h

--- motor.h
+++ motor.h
@@ -2,3 +2,15 @@
 
 void forwards();
 //It's a function that sets the motors fowards
+
+void stop();
+//Stops motor
+
+void setSpeed();
+//Sets motor speed
+
+void encoderRead();
+//Reads the encoder, add more detail later
+
+void motorSetup();
+//Motor setup code
