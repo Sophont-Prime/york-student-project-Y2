9e5c48de9cb89f21b2d400aa2c38892dc0fffe36
Sophont-Prime <147148655+Sophont-Prime@users.noreply.github.com>
2023-11-22 16:15:12 +0000

    Update sensor.h
    removed function

--- sensor.h
+++ sensor.h
@@ -1,10 +1,7 @@
 //sensor.h
 
-//Send byte to assigned bus
-void TCA9548A(uint8_t bus);
-
 //Runs through sensor setup (Begins wire instance to be passed to wire library)
 void sensorSetup();
 
 //Reads the passed sensor channel - notable channels are 2, 3 and 4 (R, L, F)
-void sensorRead(int channel);
+int sensorRead(int channel);
