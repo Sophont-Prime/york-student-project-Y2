44bf50d87f55741e11b1581e72ecb2d7e721db42
Sophont-Prime <147148655+Sophont-Prime@users.noreply.github.com>
2023-11-22 13:11:01 +0000

    Update Main.ino
    Integrated sensor setup

--- Main.ino
+++ Main.ino
@@ -1,8 +1,23 @@
 #include "motor.h"
 #include <Arduino.h>
+//Mot 1
+#define M1D1 4 //Direction1 for Motor 1
+#define M1D2 5 //Direction2 for Motor 1
+#define M1S 6 //Speed for Motor 1
+#define E1F A1
+#define E1B A2
+//Mot 2
+#define M2D1 7 //Direction1 for Motor 2
+#define M2D2 8 //Direction2 for Motor 2
+#define M2S 9 //Speed for Motor 2
+#define E2F A3
+#define E2B A4
 
 void setup(){
   Serial.begin(9600);
+  motorSetup();
+  sensorSetup();
+  Serial.println("Setup complete");
 }
 void loop(){
   forwards();
