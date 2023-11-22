a5f737fea586db6c27ce1cfca4e429020109873b
Sophont-Prime <147148655+Sophont-Prime@users.noreply.github.com>
2023-11-22 13:51:03 +0000

    Create pid.cpp

---
+++ pid.cpp
@@ -0,0 +1,26 @@
+
+int PID_memory = 10;
+int PID_H[10];
+
+float PID(int input, int target){
+  float Kp = 1;
+  float Ki = 0.0;
+  float Kd = 3;
+
+  for(int i = 0; i<PID_memory-1; ++i){
+    PID_H[i] = PID_H[i+1];
+  } 
+  PID_H[PID_memory] = input;
+  
+  int P = target - input;
+
+  int I = 0;
+  for(int i = 0; i<PID_memory; ++i){
+    I = I + PID_H[i];
+  } 
+
+  int D = (PID_H[PID_memory] - PID_H[0])/PID_memory;
+
+  float output = Kp*P + Ki*I + Kd*D;
+  return output;
+}
