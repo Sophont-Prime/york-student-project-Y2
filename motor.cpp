82f57019e6a8e54b8f6a3b0c4b618bda8e1ce978
P-Andrii <147150865+P-Andrii@users.noreply.github.com>
2023-11-22 13:28:39 +0000

    Update motor.cpp

--- motor.cpp
+++ motor.cpp
@@ -1,6 +1,18 @@
 #include <Arduino.h>
 #include "motor.h"
 
+#define M1D1 4 //Direction1 for Motor 1
+#define M1D2 5 //Direction2 for Motor 1
+#define M1S 6 //Speed for Motor 1
+#define E1F A1
+#define E1B A2
+
+#define M2D1 7 //Direction1 for Motor 2
+#define M2D2 8 //Direction2 for Motor 2
+#define M2S 9 //Speed for Motor 2
+#define E2F A6
+#define E2B A7
+
 /*
 Motors require one of the Direction pins to be High and one to be Low to define direction.
 The Speed pin must be a PWM signal that defines the speed. If the signal is too small (~30) the motor won't move
@@ -16,10 +28,12 @@ void stop(){
   digitalWrite(M2D1, LOW);
 }
 
-void setSpeed(){
-    analogWrite(M1S, 160);
-    analogWrite(M2S, 160);
-  }
+void setSpeed(int S1, int S2){
+  analogWrite(M1S, S1);
+  analogWrite(M2S, S2);
+}
+
+
 
 void encoderRead(){
   int i = 0;
@@ -43,5 +57,31 @@ void motorSetup(){
   pinMode(M2D2, OUTPUT);
   pinMode(M2S, OUTPUT);
   
-  setSpeed();
+  setSpeed(60, 60);
+}
+
+void turn_right(){
+  digitalWrite(M1D1, LOW);
+  digitalWrite(M2D1, LOW);
+  digitalWrite(M1D2, LOW);
+  digitalWrite(M2D2, LOW);
+  digitalWrite(M1D1, HIGH);
+  digitalWrite(M2D2, HIGH);
+  delay(375);
+  digitalWrite(M1D1, LOW);
+  digitalWrite(M2D2, LOW);
+
+}
+
+void turn_left(){
+  digitalWrite(M1D1, LOW);
+  digitalWrite(M2D1, LOW);
+  digitalWrite(M1D2, LOW);
+  digitalWrite(M2D2, LOW);
+  digitalWrite(M1D2, HIGH);
+  digitalWrite(M2D1, HIGH);
+  delay(375);
+  digitalWrite(M1D1, LOW);
+  digitalWrite(M2D2, LOW);
+
 }
