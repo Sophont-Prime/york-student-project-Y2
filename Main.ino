1f1cbf6fcaa88af9f08e76ee8da79d56ec3ff5ed
P-Andrii <147150865+P-Andrii@users.noreply.github.com>
2023-11-23 19:26:14 +0000

    Update Main.ino

--- Main.ino
+++ Main.ino
@@ -2,60 +2,71 @@
 #include "sensor.h"
 #include "pid.h"
 #include <Arduino.h>
-//Mot 1
-#define M1D1 4 //Direction1 for Motor 1
-#define M1D2 5 //Direction2 for Motor 1
-#define M1S 6 //Speed for Motor 1
-#define E1F A1
-#define E1B A2
-//Mot 2
-#define M2D1 7 //Direction1 for Motor 2
-#define M2D2 8 //Direction2 for Motor 2
-#define M2S 9 //Speed for Motor 2
-#define E2F A3
-#define E2B A4
 
 int Speed_init = 60;
 int left_motor = 0;
 int right_motor = 0;
 
+int PID_memory = 10;
+int PID_H[10];
+
+
+//Ridley was here
+
 void setup(){
   Serial.begin(9600);
   motorSetup();
   sensorSetup();
-  Serial.println("Setup complete");
+  Serial.println("Setup Complete");
   forwards();
 }
-void loop(){
+
+void loop() 
+{
+  // An LED and when blinking shows that the code is working
   digitalWrite(LED_BUILTIN, HIGH);
-  Serial.print("1");
+
   int right = sensorRead(2);
   int left = sensorRead(3);
   int front = sensorRead(4);
-  Serial.print("2");
+
+  
   int error = left - right;
-  Serial.print("3");
-  float adjustment = PID(error, 0)*Speed_init/55;
+
+  for(int i = 0; i<PID_memory-1; ++i){
+    PID_H[i] = PID_H[i+1];
+  } 
+  PID_H[PID_memory-1] = error;
+
+  float adjustment = PID(error, 0, PID_memory, PID_H)*Speed_init/55;
   if (adjustment < -40){
     adjustment = -40;
   }
   if (adjustment > 40){
     adjustment = 40;
   }
+
   left_motor = Speed_init + adjustment;
   right_motor = Speed_init - adjustment;
-  Serial.print("4");
+
   if (front <= 40){
-    turn_left();
-    turn_left();
+      turn_right(); // In following the left-hand wall algorithm the robot will only ever see a wall in front if it's a turn to the right 
+    }
+  if (left >= 60){
     forwards();
+    delay(225);
+    turn_left();
+    delay(750);
   }
+
+
   setSpeed(left_motor, right_motor);
   Serial.print("Left motor:");
   Serial.print(left_motor);
   Serial.print("   Right motor:");
   Serial.println(right_motor);
-  Serial.println("5");
+  
+  
   digitalWrite(LED_BUILTIN, LOW);
-  delay(25);
+  delay(50);// A delay for the blinking of the LED to be noticeable - entirely bug-fixing, could be removed later
 }
