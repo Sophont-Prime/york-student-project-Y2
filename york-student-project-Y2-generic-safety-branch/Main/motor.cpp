517acc2eb17843da6647cb91a56ad86c7e1b8ee3
Sky-Cassels <132280009+Sky-Cassels@users.noreply.github.com>
2023-12-08 16:38:41 +0000

    Update motor.cpp

--- york-student-project-Y2-generic-safety-branch/Main/motor.cpp
+++ york-student-project-Y2-generic-safety-branch/Main/motor.cpp
@@ -1,12 +1,13 @@
 #include <Arduino.h>
 #include "motor.h"
 
+//mot 1
 #define M1D1 4 //Direction1 for Motor 1
 #define M1D2 5 //Direction2 for Motor 1
 #define M1S 6 //Speed for Motor 1
 #define E1F A1
 #define E1B A2
-
+//mot 2
 #define M2D1 7 //Direction1 for Motor 2
 #define M2D2 8 //Direction2 for Motor 2
 #define M2S 9 //Speed for Motor 2
@@ -19,19 +20,22 @@ The Speed pin must be a PWM signal that defines the speed. If the signal is too 
 */
 
 void forwards(){
-  //setSpeed(60, 60);
   digitalWrite(M1D1, HIGH);
   digitalWrite(M2D1, HIGH);
   digitalWrite(M1D2, LOW);
   digitalWrite(M2D2, LOW);
 }
 
-void backwards(){
-  //setSpeed(60, 60);
+void reverse(){
+  //stop();
+  //setSpeed(30, 30);
+  stop();
   digitalWrite(M1D1, LOW);
   digitalWrite(M2D1, LOW);
   digitalWrite(M1D2, HIGH);
   digitalWrite(M2D2, HIGH);
+  delay(800);
+  forwards();
 }
 
 void stop(){
@@ -42,7 +46,6 @@ void stop(){
 }
 
 void setSpeed(int S1, int S2){
-  
   if (S1 <= 0){
     S1 = 0;
   }
@@ -55,6 +58,7 @@ void setSpeed(int S1, int S2){
   if (S2 >= 255){
     S2 = 255;
   }
+  
   analogWrite(M1S, S1);
   analogWrite(M2S, S2*1.1);
 }
@@ -82,41 +86,25 @@ void motorSetup(){
   pinMode(M2D1, OUTPUT);
   pinMode(M2D2, OUTPUT);
   pinMode(M2S, OUTPUT);
-  
-  setSpeed(60, 60);
 }
 
 void turn_right(){
-  setSpeed(50, 50);
+  //setSpeed(50, 50);
   stop();
   digitalWrite(M1D1, HIGH);
   digitalWrite(M2D2, HIGH);
-  delay(500);
+  delay(800);
   stop();
-  
   delay(150);
-  //forwards();
 }
 
 void turn_left(){
   stop();
-  setSpeed(50, 50);
   digitalWrite(M1D2, HIGH);
-  digitalWrite(M2D1, HIGH); 
-  delay(502);
+  digitalWrite(M2D1, HIGH);
+  delay(800);
   stop();
-  delay(100);
   forwards();
-  delay(500);
+  delay(1500);
+  //forwards();
 }
-
-void backupthebooty(){
-  setSpeed(30, 30);
-  stop();
-  backwards();
-  delay(400);
-  forwards();
-
-}
-
-
