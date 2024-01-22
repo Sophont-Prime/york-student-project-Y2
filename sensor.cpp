920cfe6b658809280e92166704782aa410d66de5
P-Andrii <147150865+P-Andrii@users.noreply.github.com>
2024-01-22 04:56:29 +0000

    Update sensor.cpp

--- sensor.cpp
+++ sensor.cpp
@@ -4,15 +4,14 @@
 #include "Adafruit_VL6180X.h"
 #include <Wire.h>
 
-TCA9548A I2CMux;                  // Address can be passed into the constructor
+TCA9548A I2CMux;        // Address can be passed into the constructor
 
 Adafruit_VL6180X vl = Adafruit_VL6180X();
 
 void sensorSetup(){
-  //  Wire.setPins(21, 22);       // ESP32 users, use setPins(sda, scl) if customised, *before* passing Wire to the library (the line below).  
-  I2CMux.begin(Wire);             // Wire instance is passed to the library
+  I2CMux.begin(Wire);   // Wire instance is passed to the library
 
-  I2CMux.closeAll();              // Set a base state which we know (also the default state on power on)
+  I2CMux.closeAll();    // Set a base state which we know (also the default state on power on)
 }
 
 int sensorRead(int channel){
